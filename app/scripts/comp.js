3fff3754f87354090a760a9de963c15d5495e631
Quinn <zhangc11@hhmi.org>
2016-08-18 16:58:36 -0400

    d3

--- app/scripts/comp.js
+++ app/scripts/comp.js
@@ -5,16 +5,18 @@ angular.module('mainApp' ,[])
        .component('hero', {
 		templateUrl: 'views/main.html',
 		transclude: true,
-		controller:  function (){
+		controller:  function ($scope){
 			var $ctrl = this;
 
 			$ctrl.achivement = 
 			  { 
 			    age: 24, 
-			    code: 1111111,
-			    hour: 2333333,
+			    code: 111,
+			    hour: 233,
 			    project: 5
 			  };
+  
+ 
 
 			$ctrl.skills = ['HTML','JavaScript','CSS', 'LESS', 'SCSS', 'PHP', 'Java', 'C', 'Drupal', 'WordPress', 'Angular.js', 'Git', 'Docker'];
 
@@ -22,6 +24,8 @@ angular.module('mainApp' ,[])
 
 			$ctrl.addup1 = function() {
 			  $ctrl.achivement.age += 1;
+			    Svg();
+		
 			};
 
 			$ctrl.addup2 = function() {
@@ -34,6 +38,7 @@ angular.module('mainApp' ,[])
 
 			$ctrl.addup4 = function() {
 			  $ctrl.achivement.project += 1;
+				updateSVG();
 			};
 
 			$ctrl.choose = function(){
@@ -46,8 +51,113 @@ angular.module('mainApp' ,[])
 			    $ctrl.newskills = $ctrl.newskills.concat(temp);
 			};
 
+			var Svg = function(){
+
+				var data = $.map($ctrl.achivement, function(value, index) {
+    			  return [value];
+				});
+
+		
+				var width = 420
+				   ,barHeight = 20;
+
+				var x = d3.scale.linear()
+				    .range([0, width]);
+
+				x.domain([0, d3.max(data)]);
+
+				var chart = d3.select(".chart")
+				    .attr("width", width)
+				    .attr("height", barHeight * data.length)
+			
+				var bar = chart.selectAll("g")
+				  .data(data)
+				  .enter().append("g")
+				  .attr("transform", function(d, i) { 
+				  	return "translate(0," + i * barHeight + ")"; });
+
+				bar.append("rect")
+				  .attr("width", function(d,i) {
+				  		console.log(d,i,'test');
+				  		return d;
+				  })
+				  .attr("height", barHeight - 1);
+
+				bar.append("text")
+				  .attr("x", function(d) { return x(d) - 3; })
+				  .attr("y", barHeight / 2)
+				  .attr("dy", ".35em")
+				  .text(function(d) { return d; })
+
+  		
+				  bar.transition();
+
+				function type(d) {
+				  d.value = +d.value; // coerce to number
+				  return d;
+				}
+
+			}
+
+			function updateSVG(){
+				var data = $.map($ctrl.achivement, function(value, index) {
+    			  return [value];
+				});
+
+		
+				var width = 420
+				   ,barHeight = 20;
+
+				var x = d3.scale.linear()
+				    .range([0, width]);
+
+				x.domain([0, d3.max(data)]);
+
+				var chart = d3.select(".chart")
+				    .attr("width", width)
+				    .attr("height", barHeight * data.length)
+			
+				var bar = chart.selectAll("g")
+				  .data(data)
+				  .attr("transform", function(d, i) { 
+
+				  	return "translate(0," + i * barHeight + ")"; });
+
+  				bar.select("rect")
+				  .attr("width", 1000000)
+				  .attr("height", barHeight - 1)
+
+				bar.select("text")
+				  .attr("x", function(d) { return x(d) - 3; })
+				  .attr("y", barHeight / 2)
+				  .attr("dy", ".35em")
+				  .text(function(d) { return d; })
+
+console.log(	bar.select("rect"),'rect');
+
+
+
+				  bar.transition();
+
+				function type(d) {
+				  d.value = +d.value; // coerce to number
+				  return d;
+				}
+			}
+
+  			// requestAnimationFrame(Svg);
+
    		},
     });
+
+
+
+
+
+
+
+
+
 
 angular.module('mainApp')
    .directive('heroChanger', function() {
